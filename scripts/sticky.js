4e05b07c6be3db8ea7cff6f1bfd4a1ec425945e1
Unknown <JBecker406@west-mec.org>
2018-04-11 17:49:29 -0700

    Updated Sticky
    Added javascript and styles for sticky navigation so that it works as it's meant to with ease for all platforms

--- scripts/sticky.js
+++ scripts/sticky.js
@@ -1,10 +1,15 @@
-var navbar = document.getElementsByClassName("mainNav")[0];
+var navbar = document.getElementsByTagName("nav")[0];
+var header = document.getElementsByTagName("header")[0];
 var sticky = navbar.offsetTop;
+
+window.onscroll = function() {checkForStick()};
 
 function checkForStick() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
+        header.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
+        header.classList.remove("sticky");
     }
 }
