626fcf73bfbc700da855dd316b350f50b7fba304
Unknown <JBecker406@west-mec.org>
2018-04-25 18:13:32 -0700

    Changed Carousel Circles and Began Submenus
    Added css and js to create an active circle which is displayed differently and treated differently from other circles and began creation of submenu with styles

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,6 +1,6 @@
 var slideIndex = 1;
 var slides = document.getElementsByClassName("cImage");
-var squares = document.getElementsByClassName("cSun");
+var suns = document.getElementsByClassName("cSun");
 showSlide();
 
 function rightArrow() {
@@ -26,21 +26,26 @@ function showSlide() {
             slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "block";
+    var selectedSun = suns[slideIndex - 1];
+    for (var i = 0; i < suns.length; i++) {
+        suns[i].classList.remove("cActive");
+    }
+    selectedSun.classList.add("cActive");
 }
 
-function squareSelect(e) {
-    var selectedSquare = e.target;
-    var squareNumber = selectedSquare.id.slice(6);
-    slideIndex = parseInt(squareNumber) + 1;
+function sunSelect(e) {
+    var selectedSun = e.target;
+    var sunNumber = selectedSun.id.slice(3);
+    slideIndex = parseInt(sunNumber) + 1;
     showSlide();
 }
 
 function addEventListeners() {
     document.getElementById("cPrev").addEventListener("click", leftArrow);
     document.getElementById("cNext").addEventListener("click", rightArrow);
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].addEventListener("click", squareSelect);
-        squares[i].setAttribute("id", "square" + i);
+    for (var i = 0; i < suns.length; i++) {
+        suns[i].addEventListener("click", sunSelect);
+        suns[i].setAttribute("id", "sun" + i);
     }
 }
 
